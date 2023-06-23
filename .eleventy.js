f61d75dc7478b8cb8d7e4aa18edfa6b72bec724a
Iveta Círová <cirova.iveta@ptw.cz>
2023-06-23 08:53:35 +0200

    index.html a cv.html

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports=function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("icons");
+    eleventyConfig.addPassthroughCopy("certificate");
 
     //eleventyConfig.addPassthroughCopy("**/*.js");
 
