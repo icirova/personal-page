700dad75cf00a1889756ebd072dc6d019920c872
Iveta Círová <cirova.iveta@ptw.cz>
2026-05-20 10:12:07 +0200

    Add SEO metadata and crawler files

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,14 @@
 module.exports=function (eleventyConfig) {
+    eleventyConfig.ignores.add("README.md");
+
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("icons");
     eleventyConfig.addPassthroughCopy("certificate");
     eleventyConfig.addPassthroughCopy("scripts");
     eleventyConfig.addPassthroughCopy("files");
+    eleventyConfig.addPassthroughCopy("robots.txt");
+    eleventyConfig.addPassthroughCopy("sitemap.xml");
 
     // eleventyConfig.addPassthroughCopy("**/*.js");
 
